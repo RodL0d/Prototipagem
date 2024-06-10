94725e4f9f8569dcc7f2f36bd5aa0b96ca1fed72
gabrielkopsch <bielkopsch@gmail.com>
2024-06-10 11:29:23 -0300

    tempo de jogo

---
+++ prototipagem/Assets/EnergiaVital.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnergiaVital : MonoBehaviour
+{
+    [SerializeField] float tempodevida ;
+    
+    void Start()
+    {
+       
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tempodevida = tempodevida + Time.deltaTime;
+        Debug.Log(tempodevida);
+        Morte();
+    }
+    void Morte ()
+    {
+        if (tempodevida > 10) 
+        {
+            reloadScenes();
+        }
+    }
+    void reloadScenes() 
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
