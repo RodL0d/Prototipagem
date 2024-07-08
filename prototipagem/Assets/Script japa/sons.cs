86c38e20d8d197d8754a90e34e42d113c028d6a3
Jaspiooooon <161627944+Jaspiooooon@users.noreply.github.com>
2024-07-08 10:34:38 -0300

    Ouvido bionico

--- prototipagem/Assets/Script japa/sons.cs
+++ prototipagem/Assets/Script japa/sons.cs
@@ -6,10 +6,20 @@ using UnityEngine;
 public class sons : MonoBehaviour
 {
     [SerializeField] int tiposDeSons;
-    [SerializeField] int tiposDeEncaixe;
+    
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-     if(collision.gameObject.tag||)   
+     if(collision.gameObject.tag == "Encaixe")
+        {
+            if (tiposDeSons == collision.GetComponent<TiposDeEncaixe>().tiposDeEncaixe)
+            {
+                Debug.Log(true);
+            }
+            else 
+            {
+                Debug.Log(false);
+            }
+        }   
     }
 }
