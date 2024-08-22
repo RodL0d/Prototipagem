17ceb36aeb46b6cda53f317a5b719aa9b28708d5
MatheusKamiga <161627783+MatheusKamiga@users.noreply.github.com>
2024-08-22 11:26:50 -0300

    feito

--- prototipagem/Assets/FaseFacil/ScriptsFaseFacil/BarreiraFaseFacil.cs
+++ prototipagem/Assets/FaseFacil/ScriptsFaseFacil/BarreiraFaseFacil.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class BarreiraFaseFacil : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float velocidade;
+
+    [SerializeField]Botao botao;
+    bool daniel;
+    Vector2 posicaoInicial;
+    [SerializeField] int maxposicao = 5;
+
     void Start()
     {
-        
+        posicaoInicial = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+
+
+        if (botao.pisou == true && transform.position.y <= maxposicao + posicaoInicial.y)
+        {
+            transform.Translate(Vector2.up * Time.deltaTime * velocidade);
+
+            daniel = true;
+        }
+
+        if (botao.pisou == false && daniel == true && transform.position.y >= posicaoInicial.y)
+        {
+            transform.Translate(Vector2.down * Time.deltaTime * velocidade);
+            //transform.localPosition = new Vector2(8, 3);
+        }
+
     }
 }
