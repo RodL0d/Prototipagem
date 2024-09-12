93259bc01168f16a5636048367d527a93832de40
gabrielkopsch <bielkopsch@gmail.com>
2024-09-12 11:13:49 -0300

    apagando coisas antigas kk

--- prototipagem/Assets/Script japa/TiposDeEncaixe.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class TiposDeEncaixe : MonoBehaviour
-{
-    [SerializeField] public int tiposDeEncaixe;
-}
