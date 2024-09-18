101af546dfc1d0f45f5581dd56e5ae6dcf7fd8f2
gabrielkopsch <bielkopsch@gmail.com>
2024-09-18 09:01:48 -0300

    Squashed commit of the following:
    commit 837e26aefdf7ca730da2ab18a2497e85525510dc
    Author: gabrielkopsch <bielkopsch@gmail.com>
    Date:   Wed Sep 18 08:56:24 2024 -0300
    
        apagando tudo
    
        deixei so os scripts do som e encaixe e apaguei o resto
    
    commit 96c70e5bba2764cc89304b455b0aa26f96aca298
    Author: Jaspiooooon <161627944+Jaspiooooon@users.noreply.github.com>
    Date:   Wed Aug 7 14:22:49 2024 -0300
    
        OlhoBionico

---
+++ prototipagem/Assets/Scripts Joao/sons.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class sons : MonoBehaviour
+{
+    [SerializeField] GameObject porta;
+    [SerializeField] int tiposDeSons, maxposicao;
+    Vector3 posicaoInicial;
+    bool abre;
+    float velocidade = 5;
+
+    private void Start()
+    {
+        posicaoInicial = porta.transform.position;
+    }
+
+    private void Update()
+    {
+        if (abre && porta.transform.position.y <= maxposicao + posicaoInicial.y)
+        {
+            porta.transform.Translate(Vector2.up * Time.deltaTime * velocidade);
+        }
+        else if(!abre && porta.transform.position.y >= posicaoInicial.y)
+        {
+            Debug.Log(false);
+            porta.transform.Translate(Vector2.down * Time.deltaTime * velocidade);
+            //transform.localPosition = new Vector2(8, 3)
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+            if (collision.gameObject.tag == "Encaixe")
+            {
+                if (tiposDeSons == collision.GetComponent<TiposDeEncaixe>().tiposDeEncaixe)
+                {
+                    abre = true;
+                }
+            }
+    }
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.gameObject.tag == "Encaixe")
+            {
+                if (tiposDeSons == collision.GetComponent<TiposDeEncaixe>().tiposDeEncaixe)
+                {
+                    abre = false;
+                }
+            }
+        }
+    }
