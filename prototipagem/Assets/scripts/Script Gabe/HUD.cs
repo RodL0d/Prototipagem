dc5d6c16fc59b6a4734608be5e4a91236a8f5e5a
gabrielkopsch <bielkopsch@gmail.com>
2024-09-23 09:20:33 -0300

    modificaçao na cena

--- prototipagem/Assets/scripts/Script Gabe/HUD.cs
+++ prototipagem/Assets/scripts/Script Gabe/HUD.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HUD : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] Image healthBar;
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateHealthBar(float currentHealth, float maxHealth)
     {
-        
+        healthBar.fillAmount = currentHealth / maxHealth;
     }
 }
