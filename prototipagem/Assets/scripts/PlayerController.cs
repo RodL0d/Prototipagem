0ba36f7091d6397517e6c1f08fcd359dee352786
gabrielkopsch <161628266+gabrielkopsch@users.noreply.github.com>
2024-11-25 21:47:07 -0300

    finalizando a fase 5

--- prototipagem/Assets/scripts/PlayerController.cs
+++ prototipagem/Assets/scripts/PlayerController.cs
@@ -93,7 +93,7 @@ public class PlayerController : MonoBehaviour
         Movement();
         SuperJump();
         //Passarfase();
-        //RetrocederFase();
+       // RetrocederFase();
 
         if (playerCollider.OnGround && rb.velocity.y <= 0)
         {
